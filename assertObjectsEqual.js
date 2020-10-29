59be95f6575c094d219c6e0a569a3fd2840dc534
Sebastian <lynchseb@gmail.com>
2020-10-29 16:49:45 +0000

    added an assertObjectsEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -19,3 +19,32 @@ const eqObjects = function(obj1, obj2) {
   //Loop ends and therefore the objects are equivalent. 
   return true;
 };
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    // console.log(array1[i], array2[i])
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+  
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect
+  let equal = `✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
+  let unequal = `🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
+  if(eqObjects(actual, expected)){
+    return equal;
+  } else return unequal;
+};
+
+console.log(assertObjectsEqual({ b: "2", a: "1"}, { a: "1", b: "2" }))
+console.log(assertObjectsEqual({ b: "2", a: "1", d: "4"}, { d: "4", a: "1", b: "2" }))
+console.log(assertObjectsEqual({ b: "2", a: "1"}, { a: "1", b: "2", c: "3" }))
+console.log(assertObjectsEqual({ b: "2", a: "1"}, { a: "1", b: "2", c: "3" }))
+
