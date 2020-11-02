39497f1cd7299fe4fa7427b44e1be1fd38897b80
Sebastian <lynchseb@gmail.com>
2020-11-02 19:13:21 +0000

    implemented test directory and tests for assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return false;
 
   } else if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+    return true;
   }
 };
-// TEST CODE
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-
-  } else if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const head = function(array) {
   if (array.length < 0) {
@@ -15,9 +8,6 @@ const head = function(array) {
 
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5.5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "hello");
-// console.log(head(['hello', 'there']))
-// console.log(head([5]))
+module.exports = head;
+
+
