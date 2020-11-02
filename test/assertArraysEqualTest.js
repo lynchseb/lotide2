7c61f6ad458b87752e87e9f947e1ef38b6f16cd0
Sebastian <lynchseb@gmail.com>
2020-11-02 19:32:39 +0000

    implemented assertArraysEqualTest.js

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('../assertArraysEqual')
+
+console.log(assertArraysEqual(['hi', 2, 3], ['hi', 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "5"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -6,6 +6,5 @@ console.log(eqArrays(['hi', 2, 3], ['hi', 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3", "5"])); // => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
 console.log(assertEqual(eqArrays([1, 2, 3], [1, '2', 3]), false));
